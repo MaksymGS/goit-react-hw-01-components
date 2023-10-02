df1e4637a600610ae11cb6db8141841497e84640
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-10-02 21:19:17 +0300

    2nd done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,16 +4,30 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   console.log(title);
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.list}>
         {stats.map(item => (
-          <li key={item.id} className="item">
-            <span className="label">{item.label}</span>
-            <span className="percentage">{item.percentage}</span>
+          <li
+            style={{
+              backgroundColor: getRandomColor(),
+            }}
+            key={item.id}
+            className={css.item}
+          >
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}</span>
           </li>
         ))}
       </ul>
     </section>
   );
 };
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
